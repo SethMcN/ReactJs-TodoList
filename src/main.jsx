4b66b6e543cee7ff63f0ca6a913823409d29f90a
SethMcn <107069935+SethMcN@users.noreply.github.com>
2024-12-07 15:51:56 +0000

    Replace TodoStyle.css with style.css for improved organization and update index.html to link the new stylesheet

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import "./TodoStyle.css"
 import App from './App.jsx'
+import './style.css'
+
 
 createRoot(
   document.getElementById('root')
